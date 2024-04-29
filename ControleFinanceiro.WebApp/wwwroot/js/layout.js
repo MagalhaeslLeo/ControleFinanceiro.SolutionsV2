d88ea90034b71697bf9dfade4c8e19d553bdba2e
MagalhaeslLeo <111078752+MagalhaeslLeo@users.noreply.github.com>
2024-04-29 20:38:15 -0300

    Definição layout

--- ControleFinanceiro.WebApp/wwwroot/js/layout.js
+++ ControleFinanceiro.WebApp/wwwroot/js/layout.js
@@ -1,4 +1,6 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿$('#modulo-cadastro').click(function () {
+    $('#funcao-cadastro').css("display", "block");
+});
+$('#modulo-adm').click(function () {
+    $('#funcao-administrativo').css("display", "block");
+});
