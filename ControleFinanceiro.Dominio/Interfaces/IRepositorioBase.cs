9595bdc66709a5328925318dde080b40d57a50da
MagalhaeslLeo <111078752+MagalhaeslLeo@users.noreply.github.com>
2024-05-01 12:51:45 -0300

    Implementando repositório

--- ControleFinanceiro.Dominio/Interfaces/IRepositorioBase.cs
+++ ControleFinanceiro.Dominio/Interfaces/IRepositorioBase.cs
@@ -9,8 +9,10 @@ namespace ControleFinanceiro.Dominio.Interfaces
 {
     public interface IRepositorioBase<T> where T : IDEntidade
     {
-        Task Adicionar(T entidade);
-        Task AdicionarSalvar(T entidade);
+        IQueryable<T> Queryable();
+        Task Commit();
+        void Adicionar(T entidade);
+        Task<T> AdicionarSalvar(T entidade);
         Task<IEnumerable<T>> ObterTodos();
         Task<T> ObterPorID(Guid Id);
         Task<T> Atualizar(T entidade);
