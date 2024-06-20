5129c9e2a888ae83d04c88bfffc92df30b813151
MagalhaeslLeo <111078752+MagalhaeslLeo@users.noreply.github.com>
2024-06-20 12:42:30 -0300

    Adicionado total despesa e receita e spinner de carregamento entre janelas

--- ControleFinanceiro.WebApp/wwwroot/js/ModalTabela.js
+++ ControleFinanceiro.WebApp/wwwroot/js/ModalTabela.js
@@ -1,4 +1,21 @@
 ﻿$('.botaoMostrarModal').on('click', function () {
     var targetId = $(this).data('target');
     $(targetId).modal('show');
-})
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    function showSpinner() {
+        var spinner = document.getElementById('spinner');
+        spinner.classList.remove('d-none');
+    }
+    function hideSpinner() {
+        var spinner = document.getElementById('spinner');
+        spinner.classList.add('d-none');
+    }
+    showSpinner();
+
+    window.addEventListener('load', function () {
+        hideSpinner();
+    })
+});
+
