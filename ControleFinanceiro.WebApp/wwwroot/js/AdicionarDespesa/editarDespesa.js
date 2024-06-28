7505cc8029ff20909c30f3b6ef56a4e6d7583f40
MagalhaeslLeo <111078752+MagalhaeslLeo@users.noreply.github.com>
2024-06-28 12:14:27 -0300

    Finalizando validações receita e despesa

--- ControleFinanceiro.WebApp/wwwroot/js/AdicionarDespesa/editarDespesa.js
+++ ControleFinanceiro.WebApp/wwwroot/js/AdicionarDespesa/editarDespesa.js
@@ -1,86 +1,95 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
 
+
+    var forms = document.querySelectorAll('form.editar-despesa');
+
     //Validações dos campos do formulário
-    var form = document.getElementById('frmEditar');
-    var descricao = document.getElementById('descricaoEditar');
-    var periodo = document.getElementById('periodoEditar');
-    var valor = document.getElementById('valorEditar');
-    var submitButton = document.getElementById('submitButtonEditar');
+    forms.forEach(function (form) {
+        var descricao = form.querySelector('[id^="descricaoEditar"]');
+        var periodo = form.querySelector('[id^="periodoEditar"]');
+        var valor = form.querySelector('[id^="valorEditar"]');
+        var submitButton = form.querySelector('[id^="submitButtonEditar"]');
+
+        //Funções de validação
+        function atualizarSubmitButton() {
+            submitButton.disabled = !(validacaoDescricao() && validacaoValor && validacaoPeriodo());
+        };
+
+        function validacaoFormulario() {
+            var validado = true;
+            validado = validacaoDescricao() && validado;
+            validado = validacaoValor() && validado;
+            validado = validacaoPeriodo() && validado;
+            submitButton.disabled = !validado;
+            return validado;
+        };
+
+        function validacaoDescricao() {
+            var descricaoErro = form.querySelector('[id^=descricaoErroEditar]');
+            if (descricao.value.trim() === '') {
+                descricao.classList.add('is-invalid');
+                descricaoErro.textContent = 'Informe a descrição';
+                return false;
+            }
+            else {
+                descricao.classList.remove('is-invalid');
+                descricaoErro.textContent = '';
+                return true;
+            }
+        }
+
+        function validacaoValor() {
+            var valorErro = form.querySelector('[id^=valorErroEditar]');
+            if (valor.value === "0,00" || valor.value === '') {
+                valor.classList.add('is-invalid');
+                valorErro.textContent = 'Informe o valor';
+                return false;
+            }
+            else {
+                valor.classList.remove('is-invalid');
+                valorErro.textContent = '';
+                return true;
+            }
+        }
+
+        function validacaoPeriodo() {
+            var periodoErro = form.querySelector('[id^=periodoErroEditar]');
+            if (periodo.value === '') {
+                periodo.classList.add('is-invalid');
+                periodoErro.textContent = 'Informe a data';
+                return false;
+            }
+            else {
+                periodo.classList.remove('is-invalid');
+                periodoErro.textContent = '';
+                return true;
+            }
+        }
+
+        //Verifica se os elementos existem antes de adicionar os eventos
+        if (descricao && valor && periodo && submitButton) {
+            descricao.addEventListener('input', validacaoDescricao);
+            descricao.addEventListener('blur', validacaoDescricao);
+
+            valor.addEventListener('input', validacaoValor);
+            valor.addEventListener('blur', validacaoValor);
+
+            periodo.addEventListener('input', validacaoPeriodo);
+            periodo.addEventListener('blur', validacaoPeriodo);
 
 
-    descricao.addEventListener('input', validacaoDescricao);
-    descricao.addEventListener('blur', validacaoDescricao);
+            descricao.addEventListener('input', atualizarSubmitButton);
+            valor.addEventListener('input', atualizarSubmitButton);
+            periodo.addEventListener('input', atualizarSubmitButton);
 
-    valor.addEventListener('input', validacaoValor);
-    valor.addEventListener('blur', validacaoValor);
-
-    periodo.addEventListener('input', validacaoPeriodo);
-    periodo.addEventListener('blur', validacaoPeriodo);
-
-    function atualizarSubmitButton() {
-        submitButton.disabled = !(validacaoDescricao() && validacaoValor && validacaoPeriodo());
-    };
-
-    descricao.addEventListener('input', atualizarSubmitButton);
-    valor.addEventListener('input', atualizarSubmitButton);
-    periodo.addEventListener('input', atualizarSubmitButton);
-
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        var validacao = validacaoFormulario();
-        if (validacao) {
-            form.submit();
+            form.addEventListener('submit', function (event) {
+                event.preventDefault();
+                var validacao = validacaoFormulario();
+                if (validacao) {
+                    form.submit();
+                }
+            });
         }
     });
-
-    function validacaoFormulario() {
-        var validado = true;
-        validado = validacaoDescricao() && validado;
-        validado = validacaoValor() && validado;
-        validado = validacaoPeriodo() && validado;
-        submitButton.disabled = !validado;
-        return validado;
-    };
-
-    function validacaoDescricao() {
-        var descricaoErro = document.getElementById('descricaoErroEditar');
-        if (descricao.value.trim() === '') {
-            descricao.classList.add('is-invalid');
-            descricaoErro.textContent = 'Informe a descrição';
-            return false;
-        }
-        else {
-            descricao.classList.remove('is-invalid');
-            descricaoErro.textContent = '';
-            return true;
-        }
-    }
-
-    function validacaoValor() {
-        var valorErro = document.getElementById('valorErroEditar');
-        if (valor.value === "0,00" || valor.value === '') {
-            valor.classList.add('is-invalid');
-            valorErro.textContent = 'Informe o valor';
-            return false;
-        }
-        else {
-            valor.classList.remove('is-invalid');
-            valorErro.textContent = '';
-            return true;
-        }
-    }
-
-    function validacaoPeriodo() {
-        var periodoErro = document.getElementById('periodoErroEditar');
-        if (periodo.value === '') {
-            periodo.classList.add('is-invalid');
-            periodoErro.textContent = 'Informe a data';
-            return false;
-        }
-        else {
-            periodo.classList.remove('is-invalid');
-            periodoErro.textContent = '';
-            return true;
-        }
-    }
+    
 });
--- ControleFinanceiro.WebApp/wwwroot/js/AdicionarReceita/editarReceita.js
+++ ControleFinanceiro.WebApp/wwwroot/js/AdicionarReceita/editarReceita.js
@@ -1,86 +1,92 @@
 ﻿document.addEventListener('DOMContentLoaded', function () {
 
-    //Validações dos campos do formulário
-    var form = document.getElementById('frmEditar');
-    var descricao = document.getElementById('descricaoEditar');
-    var periodo = document.getElementById('periodoEditar');
-    var valor = document.getElementById('valorEditar');
-    var submitButton = document.getElementById('submitButtonEditar');
+    //Seleciona todos os formulários de edição
+    var forms = document.querySelectorAll('form.editar-receita');
 
+    //Percorre todos os formulários obtidos para as validações dos campos do formulário
+    forms.forEach(function (form) {
 
-    descricao.addEventListener('input', validacaoDescricao);
-    descricao.addEventListener('blur', validacaoDescricao);
+        var descricao = form.querySelector('[id^="descricaoEditar"]');
+        var periodo = form.querySelector('[id^="periodoEditar"]');
+        var valor = form.querySelector('[id^="valorEditar"]');
+        var submitButton = form.querySelector('[id^="submitButtonEditar"]');
 
-    valor.addEventListener('input', validacaoValor);
-    valor.addEventListener('blur', validacaoValor);
+        //Funções de validação
+        function atualizarSubmitButton() {
+            submitButton.disabled = !(validacaoDescricao() && validacaoValor && validacaoPeriodo());
+        };
 
-    periodo.addEventListener('input', validacaoPeriodo);
-    periodo.addEventListener('blur', validacaoPeriodo);
+        function validacaoFormulario() {
+            var validado = true;
+            validado = validacaoDescricao() && validado;
+            validado = validacaoValor() && validado;
+            validado = validacaoPeriodo() && validado;
+            submitButton.disabled = !validado;
+            return validado;
+        };
 
-    function atualizarSubmitButton() {
-        submitButton.disabled = !(validacaoDescricao() && validacaoValor && validacaoPeriodo());
-    };
+        function validacaoDescricao() {
+            var descricaoErro = form.querySelector('[id^="descricaoErroEditar"]');
+            if (descricao.value.trim() === '') {
+                descricao.classList.add('is-invalid');
+                descricaoErro.textContent = 'Informe a descrição';
+                return false;
+            }
+            else {
+                descricao.classList.remove('is-invalid');
+                descricaoErro.textContent = '';
+                return true;
+            }
+        }
 
-    descricao.addEventListener('input', atualizarSubmitButton);
-    valor.addEventListener('input', atualizarSubmitButton);
-    periodo.addEventListener('input', atualizarSubmitButton);
+        function validacaoValor() {
+            var valorErro = form.querySelector('[id^="valorErroEditar"]');
+            if (valor.value === "0,00" || valor.value === '') {
+                valor.classList.add('is-invalid');
+                valorErro.textContent = 'Informe o valor';
+                return false;
+            }
+            else {
+                valor.classList.remove('is-invalid');
+                valorErro.textContent = '';
+                return true;
+            }
+        }
 
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        var validacao = validacaoFormulario();
-        if (validacao) {
-            form.submit();
+        function validacaoPeriodo() {
+            var periodoErro = form.querySelector('[id^="periodoErroEditar"]');
+            if (periodo.value === '') {
+                periodo.classList.add('is-invalid');
+                periodoErro.textContent = 'Informe a data';
+                return false;
+            }
+            else {
+                periodo.classList.remove('is-invalid');
+                periodoErro.textContent = '';
+                return true;
+            }
+        }
+        if (descricao && valor && periodo && atualizarSubmitButton) {
+            descricao.addEventListener('input', validacaoDescricao);
+            descricao.addEventListener('blur', validacaoDescricao);
+
+            valor.addEventListener('input', validacaoValor);
+            valor.addEventListener('blur', validacaoValor);
+
+            periodo.addEventListener('input', validacaoPeriodo);
+            periodo.addEventListener('blur', validacaoPeriodo);
+
+            descricao.addEventListener('input', atualizarSubmitButton);
+            valor.addEventListener('input', atualizarSubmitButton);
+            periodo.addEventListener('input', atualizarSubmitButton);
+
+            form.addEventListener('submit', function (event) {
+                event.preventDefault();
+                var validacao = validacaoFormulario();
+                if (validacao) {
+                    form.submit();
+                }
+            });
         }
     });
-
-    function validacaoFormulario() {
-        var validado = true;
-        validado = validacaoDescricao() && validado;
-        validado = validacaoValor() && validado;
-        validado = validacaoPeriodo() && validado;
-        submitButton.disabled = !validado;
-        return validado;
-    };
-
-    function validacaoDescricao() {
-        var descricaoErro = document.getElementById('descricaoErroEditar');
-        if (descricao.value.trim() === '') {
-            descricao.classList.add('is-invalid');
-            descricaoErro.textContent = 'Informe a descrição';
-            return false;
-        }
-        else {
-            descricao.classList.remove('is-invalid');
-            descricaoErro.textContent = '';
-            return true;
-        }
-    }
-
-    function validacaoValor() {
-        var valorErro = document.getElementById('valorErroEditar');
-        if (valor.value === "0,00" || valor.value === '') {
-            valor.classList.add('is-invalid');
-            valorErro.textContent = 'Informe o valor';
-            return false;
-        }
-        else {
-            valor.classList.remove('is-invalid');
-            valorErro.textContent = '';
-            return true;
-        }
-    }
-
-    function validacaoPeriodo() {
-        var periodoErro = document.getElementById('periodoErroEditar');
-        if (periodo.value === '') {
-            periodo.classList.add('is-invalid');
-            periodoErro.textContent = 'Informe a data';
-            return false;
-        }
-        else {
-            periodo.classList.remove('is-invalid');
-            periodoErro.textContent = '';
-            return true;
-        }
-    }
 });
